72dd6bef887c524788a28bddf86a178ebbb3e216
Jonathan Gamble <jdgamble555@gmail.com>
2025-10-06 21:58:49 -0500

    logs

--- src/lib/firebase/firebase-jwt.ts
+++ src/lib/firebase/firebase-jwt.ts
@@ -190,24 +190,12 @@ export async function signJWT(
 
     try {
 
-        const pemKey = private_key.replace(/\\n/g, '\n');
+        const key = await importPKCS8(private_key.replace(/\\n/g, '\n'), 'RS256');
 
-        // Remove PEM headers and decode base64
-        const pemContents = pemKey
-            .replace('-----BEGIN PRIVATE KEY-----', '')
-            .replace('-----END PRIVATE KEY-----', '')
-            .replace(/\s/g, '');
-
-        const binaryKey = Uint8Array.from(atob(pemContents), c => c.charCodeAt(0));
-
-        // Import using Web Crypto API directly
-        const key = await crypto.subtle.importKey(
-            'pkcs8',
-            binaryKey,
-            { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' },
-            false,
-            ['sign']
-        );
+        console.log('Key type:', key.constructor.name);
+        console.log('Key algorithm:', key.algorithm);
+        console.log('Key usages:', key.usages);
+        console.log('Key extractable:', key.extractable);
 
         const token = await new SignJWT({ scope: SCOPES.join(' ') })
             .setProtectedHeader({ alg: 'RS256', typ: 'JWT' })
